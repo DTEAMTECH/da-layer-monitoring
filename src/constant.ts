15cec6a947b3d1c80edb0bdb2120b651e4c3c55a
d <doodleoocrypto@gmail.com>
2025-05-23 17:49:45 +0300

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -4,3 +4,4 @@ export const SYNC_TIME_CHECK = "10m";
 export const OUT_OF_SYNC_HEIGHT_THRESHOLD = 50;
 // The time in minutes to consider an alert recent, to avoid spamming the user alerts
 export const TIME_RECENT_ALERT_IN_MINUTES = 120;
+export const CONSECUTIVE_ALERTS_THRESHOLD = 3;
